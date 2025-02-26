4ce73ab500fbe0a18df0a01ffb15f905684f984f
= <rajput.gaurav8341@gmail.com>
2025-02-26 23:17:24 +0530

    deploy the changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<ProfilePage fileUrl={"/content/about.md"} />} />
+          <Route path="/profile" element={<ProfilePage fileUrl={"/content/about.md"} />} />
+          {/* <Route path="/blogs" element={<ProfilePage fileUrl={"/content/blogs.md"} />} /> */}
+          {/* <Route path="/projects" element={<ProfilePage fileUrl={"/content/projects.md"} />} /> */}
 
         </Routes>
       </Router>
--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -5,15 +5,30 @@ import './Header.css';
 const Header = () => {
     console.log('Header.js');
     return (
-        <header>
-            <nav className="navbar">
-                <ul className="navbar-menu">
-                    <li className="navbar-item"><Link to="/" className="navbar-link">Main Page</Link></li>
-                    <li className="navbar-item"><Link to="/profile" className="navbar-link">About Me</Link></li>
-                    <li className="navbar-item"><Link to="/blogs" className="navbar-link">Blog</Link></li>
-                    <li className="navbar-item"><Link to="/projects" className="navbar-link">Project</Link></li>
+        // <header>
+        //     <nav className="navbar">
+        //         <ul className="navbar-menu">
+        //             <li className="navbar-item"><Link to="/" className="navbar-link">Main Page</Link></li>
+        //             <li className="navbar-item"><Link to="/profile" className="navbar-link">About Me</Link></li>
+        //             <li className="navbar-item"><Link to="/blogs" className="navbar-link">Blog</Link></li>
+        //             <li className="navbar-item"><Link to="/projects" className="navbar-link">Project</Link></li>
+        //         </ul>
+        //     </nav>
+        // </header>
+        <header class="site-header" role="banner">
+            <div class="wrapper">
+                <ul className='navbar-menu'>
+                    <Link class="site-title" to="/">Gaurav Rajput</Link>
                 </ul>
-            </nav>
+                <nav class="site-nav">
+                    <div class="trigger">
+                        <li className='navbar-item'><Link class="page-link" to="/profile">About</Link></li>
+                        <li className='navbar-item'><Link class="page-link" to="/blogs">Blog</Link></li>
+                        <li className='navbar-item'><Link class="page-link" to="/projects">Projects</Link></li>
+                        <li className='navbar-item'><Link class="page-link" to="/contact">Contact</Link></li>
+                    </div>
+                </nav>
+            </div>
         </header>
     );
 };
