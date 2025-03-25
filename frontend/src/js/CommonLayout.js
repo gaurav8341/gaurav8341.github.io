2d087b25a205f52ba2c23bf3d6418a67697a9516
= <rajput.gaurav8341@gmail.com>
2025-03-25 14:08:35 +0530

    vercel analytics

--- frontend/src/js/CommonLayout.js
+++ frontend/src/js/CommonLayout.js
@@ -1,11 +1,18 @@
 import React from 'react';
+
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 import Header from './Header';
 import '../css/base.css';
 import Footer from './Footer';
 
 const CommonLayout = ({ children }) => {
   return (
+    
     <div className='base-container'>
+        <Analytics/>
+        <SpeedInsights/>
         <Header />
         <div className='base-layout'>
             <main>{children}</main>
